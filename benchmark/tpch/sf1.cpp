1324843af2c26de0dab632b5cd56e422e52ccc33
Sam Ansmink <samansmink@hotmail.com>
2020-10-27 12:56:00 +0100

    added custom query option

--- benchmark/tpch/sf1.cpp
+++ benchmark/tpch/sf1.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 #define TPCH_QUERY_BODY(QNR)                                                                                           \
 	virtual void Load(DuckDBBenchmarkState *state) {                                                                   \
-		if (!this->fast_mode) { 																					\
+		if (!this->fast_mode) { 																					   \
 		if (!BenchmarkRunner::TryLoadDatabase(state->db, "tpch")) {                                                    \
 			tpch::dbgen(SF, state->db);                                                                                \
 			BenchmarkRunner::SaveDatabase(state->db, "tpch");                                                          \
@@ -19,18 +19,26 @@ using namespace std;
 	}                                                                                                                  \
 	}                                                                                                                  \
 	virtual string GetQuery() {                                                                                        \
-		return tpch::get_query(QNR);                                                                                   \
+		if (QNR == -1){													                                               \
+			return BenchmarkRunner::GetInstance().custom_query;		                                                   \
+		} else {																									   \
+			return tpch::get_query(QNR);                                                                               \
+		}																											   \
 	}                                                                                                                  \
 	virtual string VerifyResult(QueryResult *result) {                                                                 \
 		if (!result->success) {                                                                                        \
 			return result->error;                                                                                      \
 		}                                                                                                              \
-		if (DISABLE_ANSWERCHECK) return "";                                                                          \
+		if (DISABLE_ANSWERCHECK) return "";                                                                            \
 		return compare_csv(*result, tpch::get_answer(SF, QNR), true);                                                  \
 	}                                                                                                                  \
 	virtual string BenchmarkInfo() {                                                                                   \
 		return StringUtil::Format("TPC-H Q%d SF%d: %s", QNR, SF, tpch::get_query(QNR).c_str());                        \
 	}
+
+DUCKDB_BENCHMARK(Q_CUSTOM, "[tpch-sf1]")
+TPCH_QUERY_BODY(-1);
+FINISH_BENCHMARK(Q_CUSTOM)
 
 DUCKDB_BENCHMARK(Q01, "[tpch-sf1]")
 TPCH_QUERY_BODY(1);
