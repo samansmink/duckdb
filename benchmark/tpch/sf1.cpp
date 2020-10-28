10f9c161a2e41e878c346392ddd5ebeeb6fc7fa7
Sam Ansmink <samansmink@hotmail.com>
2020-10-28 19:53:21 +0100

    made sf dynamic for tpch

--- benchmark/tpch/sf1.cpp
+++ benchmark/tpch/sf1.cpp
@@ -6,14 +6,13 @@
 using namespace duckdb;
 using namespace std;
 
-#define SF 1
 #define DISABLE_ANSWERCHECK 1
 
 #define TPCH_QUERY_BODY(QNR)                                                                                           \
 	virtual void Load(DuckDBBenchmarkState *state) {                                                                   \
 		if (!this->fast_mode) { 																					   \
 		if (!BenchmarkRunner::TryLoadDatabase(state->db, "tpch")) {                                                    \
-			tpch::dbgen(SF, state->db);                                                                                \
+			tpch::dbgen(this->scale_factor, state->db);                                                                \
 			BenchmarkRunner::SaveDatabase(state->db, "tpch");                                                          \
 		}                                                                                                              \
 	}                                                                                                                  \
@@ -30,10 +29,10 @@ using namespace std;
 			return result->error;                                                                                      \
 		}                                                                                                              \
 		if (DISABLE_ANSWERCHECK) return "";                                                                            \
-		return compare_csv(*result, tpch::get_answer(SF, QNR), true);                                                  \
+		return compare_csv(*result, tpch::get_answer(this->scale_factor, QNR), true);                                  \
 	}                                                                                                                  \
 	virtual string BenchmarkInfo() {                                                                                   \
-		return StringUtil::Format("TPC-H Q%d SF%d: %s", QNR, SF, tpch::get_query(QNR).c_str());                        \
+		return StringUtil::Format("TPC-H Q%d SF%d: %s", QNR, this->scale_factor, tpch::get_query(QNR).c_str());        \
 	}
 
 DUCKDB_BENCHMARK(Q_CUSTOM, "[tpch-sf1]")
