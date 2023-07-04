e4d9d97dbbb73c81623edbac2dbf3b5e94a666e1
Sam Ansmink <samansmink@hotmail.com>
2023-07-04 21:51:53 +0200

    try out on ci

--- .github/config/vcpkg_extensions.cmake
+++ .github/config/vcpkg_extensions.cmake
@@ -1,18 +1,18 @@
+##
+## This is the DuckDB extension config for extensions that require VCPKG as it will run on the CI.
+##
+## to build duckdb with this configuration run:
+##   EXTENSION_CONFIGS=.github/config/vcpkg_extensions.cmake make
+##
 #
-# This is the DuckDB extension config for extensions that require VCPKG as it will run on the CI.
+#duckdb_extension_load(azure
+#    LOAD_TESTS
+#    GIT_URL https://github.com/samansmink/azure-extension
+#    GIT_TAG 8b6f1062988cce067a54c9f1a93bf0874531a675
+#)
 #
-# to build duckdb with this configuration run:
-#   EXTENSION_CONFIGS=.github/config/vcpkg_extensions.cmake make
-#
-
-duckdb_extension_load(azure
-    LOAD_TESTS
-    GIT_URL https://github.com/samansmink/azure-extension
-    GIT_TAG 8b6f1062988cce067a54c9f1a93bf0874531a675
-)
-
-duckdb_extension_load(aws
-    LOAD_TESTS
-    GIT_URL https://github.com/samansmink/aws
-    GIT_TAG ff226f67941d71f9e20eaba031d1088366be75b1
-)
+#duckdb_extension_load(aws
+#    LOAD_TESTS
+#    GIT_URL https://github.com/samansmink/aws
+#    GIT_TAG ff226f67941d71f9e20eaba031d1088366be75b1
+#)
