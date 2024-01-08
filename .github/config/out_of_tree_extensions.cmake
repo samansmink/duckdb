f1548a45c9ec978451f69057c7d6ed4ccfcdc7d4
Sam Ansmink <samansmink@hotmail.com>
2024-01-08 21:25:47 +0100

    disable spatial

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -54,14 +54,14 @@ duckdb_extension_load(postgres_scanner
         )
 
 ################# SPATIAL
-duckdb_extension_load(spatial
-        DONT_LINK LOAD_TESTS
-        GIT_URL https://github.com/duckdb/duckdb_spatial.git
-        GIT_TAG a86c504d60e0f4400564c0f2d633547f39feef2c
-        INCLUDE_DIR spatial/include
-        TEST_DIR test/sql
-        APPLY_PATCHES
-        )
+#duckdb_extension_load(spatial
+#        DONT_LINK LOAD_TESTS
+#        GIT_URL https://github.com/duckdb/duckdb_spatial.git
+#        GIT_TAG a86c504d60e0f4400564c0f2d633547f39feef2c
+#        INCLUDE_DIR spatial/include
+#        TEST_DIR test/sql
+#        APPLY_PATCHES
+#        )
 
 ################# SQLITE_SCANNER
 # Static linking on windows does not properly work due to symbol collision
