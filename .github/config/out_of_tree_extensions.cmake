444ab3bcfb22aea7dc3de3c599d37b1ea713fdd1
Sam Ansmink <samansmink@hotmail.com>
2025-05-14 14:25:51 +0200

    re-enable delta

--- .github/config/out_of_tree_extensions.cmake
+++ .github/config/out_of_tree_extensions.cmake
@@ -57,15 +57,11 @@ endif()
 ################# DELTA
 # MinGW build is not available, and our current manylinux ci does not have enough storage space to run the rust build
 # for Delta
-if (FALSE)
-if (NOT MINGW AND NOT "${OS_NAME}" STREQUAL "linux" AND NOT ${WASM_ENABLED})
+if (NOT MINGW AND NOT ${WASM_ENABLED})
     duckdb_extension_load(delta
             GIT_URL https://github.com/duckdb/duckdb-delta
-            ## TODO: GIT_TAG 90f244b3d572c1692867950b562df8183957b7a8
-            GIT_TAG 6d626173e9efa6615c25eb08d979d1372100d5db
-            APPLY_PATCHES
+            GIT_TAG e3cd7640747d899aaeed8bd1f81898585cdf24ac
     )
-endif()
 endif()
 
 ################# EXCEL
