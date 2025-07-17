7faa6220f1ed1bac54a74daeab3f0f2df38436cc
Sam Ansmink <samansmink@hotmail.com>
2025-07-17 12:49:10 +0200

    cidebug: try full build

--- .github/config/in_tree_extensions.cmake
+++ .github/config/in_tree_extensions.cmake
@@ -5,13 +5,13 @@
 #   EXTENSION_CONFIGS=.github/config/in_tree_extensions.cmake make
 #
 
-#duckdb_extension_load(autocomplete)
-#duckdb_extension_load(core_functions)
-#duckdb_extension_load(icu)
+duckdb_extension_load(autocomplete)
+duckdb_extension_load(core_functions)
+duckdb_extension_load(icu)
 duckdb_extension_load(json)
-#duckdb_extension_load(parquet)
-#duckdb_extension_load(tpcds)
-#duckdb_extension_load(tpch)
-#
-## Test extension for the upcoming C CAPI extensions
-#duckdb_extension_load(demo_capi DONT_LINK)
+duckdb_extension_load(parquet)
+duckdb_extension_load(tpcds)
+duckdb_extension_load(tpch)
+
+# Test extension for the upcoming C CAPI extensions
+duckdb_extension_load(demo_capi DONT_LINK)
